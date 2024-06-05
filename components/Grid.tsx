b472d0d54349a320fd75699263f74a89d36d3f65
yogarp <yogarizky51@gmail.com>
2024-06-05 15:34:36 +0700

    Modern Portofolio Template JSM Done

---
+++ components/Grid.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+import { gridItems } from "@/data";
+
+const Grid = () => {
+  return (
+    <section id="about">
+      <BentoGrid>
+        {gridItems.map(
+          ({
+            className,
+            description,
+            id,
+            img,
+            imgClassName,
+            spareImg,
+            title,
+          }) => (
+            <BentoGridItem
+              key={id}
+              id={id}
+              title={title}
+              description={description}
+              className={className}
+              img={img}
+              imgClassName={imgClassName}
+              spareImg={spareImg}
+            />
+          )
+        )}
+      </BentoGrid>
+    </section>
+  );
+};
+
+export default Grid;
